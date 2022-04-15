6c41c8c092c661995242453da1f7d44c2511518e
nimod7890 <nimod1234@g.skku.edu>
2022-04-15 11:01:46 +0900

    [3] training

--- 3_semantic_segmentation/pspnet.py
+++ 3_semantic_segmentation/pspnet.py
@@ -148,23 +148,89 @@ class PyramidPooling(nn.Module):
         self.width = width
 
         out_channels = int(in_channels / len(pool_sizes))
-        self.avpool = []
-        self.cbr = []
-        for pool_size in pool_sizes:
-            self.avpool.append(nn.AdaptiveAvgPool2d(output_size=pool_size))
-            self.cbr.append(
-                conv2DBatchNormRelu(in_channels, out_channels, 1, 1, 0, 1, False)
-            )
+        # self.avpool = []
+        # self.cbr = []
+        # for pool_size in pool_sizes:
+        #     self.avpool.append(nn.AdaptiveAvgPool2d(output_size=pool_size))
+        #     self.cbr.append(
+        #         conv2DBatchNormRelu(in_channels, out_channels, 1, 1, 0, 1, False)
+        #     )
+        self.avpool_1 = nn.AdaptiveAvgPool2d(output_size=pool_sizes[0])
+        self.cbr_1 = conv2DBatchNormRelu(
+            in_channels,
+            out_channels,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            dilation=1,
+            bias=False,
+        )
+
+        self.avpool_2 = nn.AdaptiveAvgPool2d(output_size=pool_sizes[1])
+        self.cbr_2 = conv2DBatchNormRelu(
+            in_channels,
+            out_channels,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            dilation=1,
+            bias=False,
+        )
+
+        self.avpool_3 = nn.AdaptiveAvgPool2d(output_size=pool_sizes[2])
+        self.cbr_3 = conv2DBatchNormRelu(
+            in_channels,
+            out_channels,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            dilation=1,
+            bias=False,
+        )
+
+        self.avpool_4 = nn.AdaptiveAvgPool2d(output_size=pool_sizes[3])
+        self.cbr_4 = conv2DBatchNormRelu(
+            in_channels,
+            out_channels,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            dilation=1,
+            bias=False,
+        )
 
     def forward(self, x):
-        outList = [x]
-        for pool, cbr in zip(self.avpool, self.cbr):
-            out = cbr(pool(x))
-            out = F.interpolate(
-                out, size=(self.height, self.width), mode="bilinear", align_corners=True
-            )
-            outList.append(out)
-        output = torch.cat(outList, dim=1)
+        # outList = [x]
+        # for pool, cbr in zip(self.avpool, self.cbr):
+        #     out = cbr(pool(x))
+        #     out = F.interpolate(
+        #         out, size=(self.height, self.width), mode="bilinear", align_corners=True
+        #     )
+        #     outList.append(out)
+        # output = torch.cat(outList, dim=1)
+
+        out1 = self.cbr_1(self.avpool_1(x))
+        out1 = F.interpolate(
+            out1, size=(self.height, self.width), mode="bilinear", align_corners=True
+        )
+
+        out2 = self.cbr_2(self.avpool_2(x))
+        out2 = F.interpolate(
+            out2, size=(self.height, self.width), mode="bilinear", align_corners=True
+        )
+
+        out3 = self.cbr_3(self.avpool_3(x))
+        out3 = F.interpolate(
+            out3, size=(self.height, self.width), mode="bilinear", align_corners=True
+        )
+
+        out4 = self.cbr_4(self.avpool_4(x))
+        out4 = F.interpolate(
+            out4, size=(self.height, self.width), mode="bilinear", align_corners=True
+        )
+
+        # 最終的に結合させる、dim=1でチャネル数の次元で結合
+        output = torch.cat([x, out1, out2, out3, out4], dim=1)
         return output
 
 
